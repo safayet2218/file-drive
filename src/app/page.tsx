c69acbced72689a112669bdf4885edf1d8b77888
safayet2218 <safayet2218@gmail.com>
2024-03-01 15:35:37 +0600

    convex get file query setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client"; 
 import { Button } from "@/components/ui/button";
 import { SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles);
   const createFile = useMutation(api.files.createFile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -19,10 +20,14 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>
+      })}
       <Button 
       onClick={()=>{
         createFile({
-          name:"hello World"
+          name:"hello World one"
         })
       }}>
         File
