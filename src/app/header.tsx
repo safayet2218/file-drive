26f6ebf220654a0cbec8fb94d97d400fe8b75cc4
safayet2218 <safayet2218@gmail.com>
2024-03-01 17:23:37 +0600

    update

---
+++ src/app/header.tsx
@@ -0,0 +1,16 @@
+import { UserButton,SignedIn, OrganizationSwitcher } from "@clerk/nextjs";
+
+export function Header() {
+    return  <div className="border-b py-4 bg-gray-50">
+        <div className="items-center container mx-auto justify-between flex">
+            <div>
+                File Drive
+            </div>
+            <div className="flex gap-2">
+            <OrganizationSwitcher />
+            <UserButton />
+            </div>
+ 
+        </div>
+    </div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client"; 
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const files = useQuery(api.files.getFiles);
+  const { organization } = useOrganization();
+  
+  const files = useQuery(api.files.getFiles, 
+    organization?.id ? { orgId:organization.id }: "skip");
+    
   const createFile = useMutation(api.files.createFile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -26,8 +30,10 @@ export default function Home() {
       })}
       <Button 
       onClick={()=>{
+        if(!organization) return ;
         createFile({
-          name:"hello World one"
+          name:"hello World one",
+          orgId:organization.id
         })
       }}>
         File
