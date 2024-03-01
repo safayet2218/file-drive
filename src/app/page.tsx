fc0ae9c5fbe7aefda3b787ad40e9f735552415ce
safayet2218 <safayet2218@gmail.com>
2024-03-01 13:04:05 +0600

    update file upload using convex api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
+"use client"; 
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const createFile = useMutation(api.files.createFile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello</Button>
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>  
+        </SignOutButton>  
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <Button 
+      onClick={()=>{
+        createFile({
+          name:"hello World"
+        })
+      }}>
+        File
+      </Button>
     </main>
   );
 }
